23861467e5038a63a9cd10fae2e640e05ed2cfbd
thanseefpp <androtsf@gmail.com>
2023-07-06 11:30:01 +0400

    setup py file func added

--- setup.py
+++ setup.py
@@ -1,28 +1,44 @@
-from typing import List
-from setuptools import find_packages,setup
+import os
+from setuptools import find_packages, setup
 
+REPO_DIR = os.path.dirname(os.path.realpath(__file__))
 
-def get_requirements(filename:str) -> List[str]:
-    """
-        This function returns a list of requirements
-    """
-    HYPHEN_E_DOT = "-e ."
-    requirements = []
-    with open(filename) as file:
-        requirements.extend(file.readlines())
-        requirements = [item.replace("\n"," ") for item in requirements]
-        
-    if HYPHEN_E_DOT in requirements:
-        requirements.remove(HYPHEN_E_DOT)
-    return requirements
+def getVersion():
+  """
+  Get version from local file.
+  """
+  with open(os.path.join(REPO_DIR, "VERSION"), "r") as versionFile:
+    return versionFile.read().strip()
 
+def parse_file(requirementFile):
+  try:
+    return [
+      line.strip()
+      for line in open(requirementFile).readlines()
+      if not line.startswith("-")
+    ]
+  except IOError:
+    return []
 
-setup(
+def findRequirements():
+  """
+  Read the requirements.txt file and parse into requirements for setup's
+  install_requirements option.
+  """
+  requirementsPath = os.path.join(REPO_DIR, "requirements.txt")
+  requirements = parse_file(requirementsPath)
+  return requirements
+
+if __name__ == "__main__":
+
+  requirements = findRequirements()
+  
+  setup(
     name='EndToEndMLProject',
-    version='0.0.1',
+    version=getVersion(),
     description='Machine Learning End-to-End Project',
     author='Thanseefpp',
     author_email='thanseefpp@gmail.com',
     packages=find_packages(),
-    install_requires=get_requirements("requirements.txt"),
+    install_requires=requirements,
     )
