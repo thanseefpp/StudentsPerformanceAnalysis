c91c21b4d899fffef1b3dabca97264bc01ad111a
thanseefpp <androtsf@gmail.com>
2023-07-27 21:07:28 +0400

    hyperparameters model done

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -30,6 +30,49 @@ class ModelTrainer:
     def __init__(self) -> None:
         self.model_trainer_config=ModelTrainerConfig()
         
+    def get_params_for_hyper_tuning(self):
+        return {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbour Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    # 'weights':['uniform','distance'],
+                    # 'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    # 'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }   
+            }
+        
     def initiate_model_train(self,train_array,test_array):
         try:
             X_train,y_train,X_test,y_test=(
@@ -39,6 +82,7 @@ class ModelTrainer:
                 test_array[:,-1]
             )
             logger.info("Splitted train and test data to X_train,y_train,X_test,y_test")
+            params = self.get_params_for_hyper_tuning()
             # Regression models dict
             models = {
                 "Random Forest": RandomForestRegressor(),
@@ -50,7 +94,7 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,params=params)
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
             ## To get best model name from dict
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logger
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 #----------------------------------------- FUNCTIONS/CLASSES -------------------------------------#
 
 def save_object(file_path, obj):
@@ -24,11 +25,15 @@ def save_object(file_path, obj):
         raise CustomException(e, sys) from e
     
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3,n_jobs=3,refit=False)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)  # Train model
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
